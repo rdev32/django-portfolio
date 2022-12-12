ea89571897a1a5f870bf9d08dc1624a5f7e4ec90
rdev32 <rdevelops32@gmail.com>
2022-12-12 07:32:09 -0500

    mysql support

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'railway',
+        'USER': 'root',
+        'PASSWORD': '33ZWNDj0JC92XXHOVSdY',
+        'HOST': 'containers-us-west-114.railway.app',
+        'PORT': '7113', 
     }
 }
 
